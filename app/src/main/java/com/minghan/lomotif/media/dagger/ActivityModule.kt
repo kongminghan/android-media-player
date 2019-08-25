f72d45e0d0f908aef32e7cac49c2bf24454e8fd6
MingHan <minghan.kong@gmail.com>
2019-08-25 18:43:11 +0800

    Add "VideoPlayerFragment"

--- app/src/main/java/com/minghan/lomotif/media/dagger/ActivityModule.kt
+++ app/src/main/java/com/minghan/lomotif/media/dagger/ActivityModule.kt
@@ -3,6 +3,7 @@ package com.minghan.lomotif.media.dagger
 import com.minghan.lomotif.media.MainActivity
 import com.minghan.lomotif.media.MovieFragment
 import com.minghan.lomotif.media.MusicFragment
+import com.minghan.lomotif.media.VideoPlayerFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -16,4 +17,8 @@ abstract class ActivityModule {
 
     @ContributesAndroidInjector
     abstract fun movieFragment(): MovieFragment
+
+    @ContributesAndroidInjector
+    abstract fun videoPlayerFragment(): VideoPlayerFragment
+
 }
