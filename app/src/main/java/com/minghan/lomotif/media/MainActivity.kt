e85e8a40e9d480226db8afd9ffca5a41bc129d7e
MingHan <minghan.kong@gmail.com>
2019-08-27 02:51:32 +0800

    Fix layout styles

--- app/src/main/java/com/minghan/lomotif/media/MainActivity.kt
+++ app/src/main/java/com/minghan/lomotif/media/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.minghan.lomotif.media
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.lifecycle.Observer
 import com.minghan.lomotif.media.extension.setupWithNavController
+import com.minghan.lomotif.media.extension.statusBarHeight
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,6 +15,18 @@ class MainActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.statusBarColor = Color.TRANSPARENT
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        with(toolbar) {
+            setPadding(0, statusBarHeight, 0, 0)
+            layoutParams.height = statusBarHeight + toolbar.layoutParams.height
+        }
 
         // setup navigation
         val navControllerLiveData = bottom_nav.setupWithNavController(
@@ -28,6 +44,14 @@ class MainActivity : DaggerAppCompatActivity() {
             it?.addOnDestinationChangedListener { _, destination, _ ->
                 // update toolbar title
                 toolbar.title = destination.label
+
+                if (destination.id == R.id.imageDetailFragment) {
+                    toolbar.visibility = View.GONE
+                    bottom_nav.visibility = View.GONE
+                } else {
+                    toolbar.visibility = View.VISIBLE
+                    bottom_nav.visibility = View.VISIBLE
+                }
             }
         })
     }
