e08c9413495c070910ad0df26ab5fc5f83e38fb3
MingHan <minghan.kong@gmail.com>
2019-08-27 02:51:47 +0800

    Update Fresco configuration

--- app/src/main/java/com/minghan/lomotif/media/MainApplication.kt
+++ app/src/main/java/com/minghan/lomotif/media/MainApplication.kt
@@ -5,6 +5,10 @@ import com.minghan.lomotif.media.dagger.DaggerMainComponent
 import com.minghan.lomotif.media.dagger.MainComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import com.facebook.imagepipeline.decoder.SimpleProgressiveJpegConfig
+import com.facebook.imagepipeline.core.ImagePipelineConfig
+
+
 
 class MainApplication : DaggerApplication() {
 
@@ -22,7 +26,14 @@ class MainApplication : DaggerApplication() {
 
     override fun onCreate() {
         super.onCreate()
-        Fresco.initialize(this)
+
+        val config = ImagePipelineConfig.newBuilder(this)
+            .setProgressiveJpegConfig(SimpleProgressiveJpegConfig())
+            .setResizeAndRotateEnabledForNetwork(true)
+            .setDownsampleEnabled(true)
+            .build()
+
+        Fresco.initialize(this, config)
         staticObject = this
     }
 
